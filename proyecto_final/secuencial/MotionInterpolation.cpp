3f1d98d397ba38fa0515b918a0495e896cfae9fe
Juan Sebastian <juarodriguezc@unal.edu.co>
2022-10-07 00:54:10 -0500

    Bug image

--- proyecto_final/secuencial/MotionInterpolation.cpp
+++ proyecto_final/secuencial/MotionInterpolation.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <iostream>
+#include <sys/time.h>
+
 #include <opencv2/opencv.hpp>
 
 // Set the number of arguments required
@@ -15,6 +17,9 @@ using namespace cv;
 
 struct OpticalVector
 {
+    float x;
+    float y;
+
     float u;
     float v;
 };
@@ -23,157 +28,21 @@ struct OpticalVector
 Mat1f getLuminance(Mat, Mat, Mat, int, int);
 
 // Prototype for the motionImage
-Mat1f getMotionImage(Mat *, Mat *, int, int);
+Mat1f getMotionImage(Mat1f, Mat1f, int, int);
 
 // Prototype for the optical flow
-OpticalVector *getOpticalFlow(Mat *, Mat *, Mat1f, int, int);
+OpticalVector *getOpticalFlow(Mat *, Mat *, int, int);
 
-void interpolateFrames(Mat *, Mat *, Mat *, OpticalVector *, int, int);
+Mat *interpolateFrames(Mat *, Mat *, int, int);
 
-int main(int argc, char **argv)
-{
-    /*Declare the strings of load and save video*/
-    char *loadPathFr1, *loadPathFr2, *savePath;
-    // Declare the Matrix to store the image
-    Mat loadIFrame1, loadIFrame2, saveImage;
-    // Declare 3 Matrix for each channel
-    Mat imageChF1[3], imageChF2[3], imageInter[3];
-    // Declare the size of each frame
-    Size frameSize;
-    // Declare the vector to merge the channels
-    std::vector<Mat> mChannels;
-
-    // Check if the number of arguments is correct
-    if (argc < R_ARGS + 1)
-    {
-        printf("Usage: ./MotionInterpolation <Load_Video_Path1> <Load_Video_Path2> <Save_Video_Path>\n");
-        return -1;
-    }
-    // Update the paths
-    loadPathFr1 = argv[1];
-    loadPathFr2 = argv[2];
-    savePath = argv[3];
-    // Load the first frame
-    loadIFrame1 = imread(loadPathFr1, IMREAD_UNCHANGED);
-    if (!loadIFrame1.data)
-    {
-        // Show error if image not loaded correctly
-        printf("No image data Frame 1\n");
-        return -1;
-    }
-    // Load the second frame
-    loadIFrame2 = imread(loadPathFr2, IMREAD_UNCHANGED);
-    if (!loadIFrame2.data)
-    {
-        // Show error if image not loaded correctly
-        printf("No image data Frame 2 \n");
-        return -1;
-    }
-    /*
-    Split the matrix into the three channels
-        ImageChannel[0] = B
-        ImageChannel[1] = G
-        ImageChannel[2] = R
-    */
-    // Split first frame
-    split(loadIFrame1, imageChF1);
-    split(loadIFrame1, imageInter);
-    // Split second frame
-    split(loadIFrame2, imageChF2);
-
-    // Get the frame dimension
-    frameSize = loadIFrame1.size();
-
-    // Apply the effect to the first frame
-    Mat1f motionImage = getMotionImage(imageChF1, imageChF2, frameSize.width, frameSize.height);
-
-    OpticalVector *opticalFlow = getOpticalFlow(imageChF1, imageChF2, motionImage, frameSize.width, frameSize.height);
-
-    /*
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 100; j++)
-        {
-            std::cout << "I: " << i << " J: " << j << "  ***  u: " << opticalFlow[i * frameSize.width + j].u << "  v: " << opticalFlow[i * frameSize.width + j].v << std::endl;
-        }
-    }
-    */
-
-    interpolateFrames(imageChF1, imageChF2, imageInter, opticalFlow, frameSize.width, frameSize.height);
-
-    /*
-    for (int i = 0; i < frameSize.height; i++)
-    {
-        for (int j = 0; j < frameSize.width; j++)
-        {
-            imageChannel[0].at<uchar>(i, j) = 255;
-            imageChannel[1].at<uchar>(i, j) = 0;
-        }
-    }
-    */
-
-    // Save channels into the vector
-    mChannels = {imageInter[0], imageInter[1], imageInter[2]};
-    // Merge the channels
-    merge(mChannels, saveImage);
-
-    // writing the image to a defined location as JPEG
-    if (imwrite(savePath, saveImage) == false)
-    {
-        std::cout << "Saving the image, FAILED" << std::endl;
-        return -1;
-    }
-
-    //imshow("Blue Channel", saveImage);
-
-    //waitKey(0);
-    return 0;
-}
-
-// Function to get the luminance
-Mat1f getLuminance(Mat B, Mat G, Mat R, int width, int height)
-{
-    Mat1f lMatrix(height, width);
-    float fB = 0.0f;
-    float fG = 0.0f;
-    float fR = 0.0f;
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            fB = (float)B.at<uchar>(i, j);
-            fG = (float)G.at<uchar>(i, j);
-            fR = (float)R.at<uchar>(i, j);
-            lMatrix.at<float>(i, j) = 0.2987f * fR + 0.5870f * fG + 0.1140f * fB;
-        }
-    }
-    return lMatrix;
-}
-
-// Function to get the motionImage
-Mat1f getMotionImage(Mat *frame1, Mat *frame2, int width, int height)
-{
-    // Declare image for the result
-    Mat1f motionImage(height, width);
-    // Calculate the luminance of each frame
-    Mat1f lFrame1 = getLuminance(frame1[0], frame1[1], frame1[2], width, height);
-    Mat1f lFrame2 = getLuminance(frame2[0], frame2[1], frame2[2], width, height);
-    // Substract the luminances
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            float fDiff = fabs((float)lFrame1.at<float>(i, j) - (float)lFrame2.at<float>(i, j));
-            motionImage.at<float>(i, j) = (fDiff >= MOTION_PRES) ? fDiff : 0.0f;
-        }
-    }
-    return motionImage;
-}
-
-OpticalVector *getOpticalFlow(Mat *frame1, Mat *frame2, Mat1f motionFrame, int width, int height)
+OpticalVector *getOpticalFlow2(Mat *frame1, Mat *frame2, int width, int height)
 {
     Mat1f lFrame1 = getLuminance(frame1[0], frame1[1], frame1[2], width, height);
     Mat1f lFrame2 = getLuminance(frame2[0], frame2[1], frame2[2], width, height);
+
+    // Get the motionFrame of the frames
+    Mat1f motionFrame = getMotionImage(lFrame1, lFrame2, width, height);
+
     // Create array of OpticalVector for the optical flow
     struct OpticalVector *optFlow = (OpticalVector *)malloc(width * height * sizeof(struct OpticalVector));
     for (int i = 0; i < height; i++)
@@ -256,15 +125,258 @@ OpticalVector *getOpticalFlow(Mat *frame1, Mat *frame2, Mat1f motionFrame, int w
     return optFlow;
 }
 
-void interpolateFrames(Mat *frame1, Mat *frame2, Mat *interFrame, OpticalVector *opticalFlow, int width, int height)
+int main(int argc, char **argv)
 {
+    // Declare the variables for time measurement
+    struct timeval tval_before, tval_after, tval_result;
+    // Declare the strings of load and save video
+    char *loadPathFr1, *loadPathFr2, *savePath;
+    // Declare the Matrix to store the image
+    Mat loadIFrame1, loadIFrame2, saveImage;
+    // Declare 3 Matrix for each channel
+    Mat imageChF1[3], imageChF2[3];
+    Mat *imageInter;
+    // Declare the size of each frame
+    Size frameSize;
+    int width, height;
+    // Declare the vector to merge the channels
+    std::vector<Mat> mChannels;
+
+    // Check if the number of arguments is correct
+    if (argc < R_ARGS + 1)
+    {
+        printf("Usage: ./MotionInterpolation <Load_Video_Path1> <Load_Video_Path2> <Save_Video_Path>\n");
+        return -1;
+    }
+    // Update the paths
+    loadPathFr1 = argv[1];
+    loadPathFr2 = argv[2];
+    savePath = argv[3];
+    // Load the first frame
+    loadIFrame1 = imread(loadPathFr1, IMREAD_UNCHANGED);
+    if (!loadIFrame1.data)
+    {
+        // Show error if image not loaded correctly
+        printf("No image data Frame 1\n");
+        return -1;
+    }
+    // Load the second frame
+    loadIFrame2 = imread(loadPathFr2, IMREAD_UNCHANGED);
+    if (!loadIFrame2.data)
+    {
+        // Show error if image not loaded correctly
+        printf("No image data Frame 2 \n");
+        return -1;
+    }
+
+    // Get start time
+    gettimeofday(&tval_before, NULL);
+
+    // Split first frame
+    split(loadIFrame1, imageChF1);
+    // Split second frame
+    split(loadIFrame2, imageChF2);
+
+    // Get the frame dimension
+    frameSize = loadIFrame1.size();
+    width = frameSize.width;
+    height = frameSize.height;
+
+    // Interpolate the frames
+
+    imageInter = interpolateFrames(imageChF1, imageChF2, frameSize.width, frameSize.height);
+
+    // Get end time
+    gettimeofday(&tval_after, NULL);
+
+    /*Calcular los tiempos en tval_result*/
+    timersub(&tval_after, &tval_before, &tval_result);
+    /*Imprimir informe*/
+    printf("------------------------------------------------------------------------------\n");
+    printf("Tiempo de ejecución: %ld.%06ld s \n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+
+    // Save channels into the vector
+    mChannels = {imageInter[0], imageInter[1], imageInter[2]};
+    // Merge the channels
+    merge(mChannels, saveImage);
+
+    // writing the image to a defined location as JPEG
+    if (imwrite(savePath, saveImage) == false)
+    {
+        std::cout << "Saving the image, FAILED" << std::endl;
+        return -1;
+    }
+
+    // imshow("Blue Channel", saveImage);
+
+    // waitKey(0);
+    return 0;
+}
+
+// Function to get the luminance
+Mat1f getLuminance(Mat B, Mat G, Mat R, int width, int height)
+{
+    // Declare the matrix to store the luminance
+    Mat1f lMatrix(height, width);
+    // Variables to store the results
+    float fB = 0.0f;
+    float fG = 0.0f;
+    float fR = 0.0f;
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            interFrame[0].at<uchar>(i + opticalFlow[i * width + j].v, j + opticalFlow[i * width + j].u) = frame1[0].at<uchar>(i, j);
-            interFrame[1].at<uchar>(i + opticalFlow[i * width + j].v, j + opticalFlow[i * width + j].u) = frame1[1].at<uchar>(i, j);
-            interFrame[2].at<uchar>(i + opticalFlow[i * width + j].v, j + opticalFlow[i * width + j].u) = frame1[2].at<uchar>(i, j);
+            fB = (float)B.at<uchar>(i, j);
+            fG = (float)G.at<uchar>(i, j);
+            fR = (float)R.at<uchar>(i, j);
+            lMatrix.at<float>(i, j) = 0.2987f * fR + 0.5870f * fG + 0.1140f * fB;
         }
     }
+    return lMatrix;
+}
+
+// Function to get the motionImage
+Mat1f getMotionImage(Mat1f lFrame1, Mat1f lFrame2, int width, int height)
+{
+    // Declare image for the result
+    Mat1f motionImage(height, width);
+    // Substract the luminances
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float fDiff = fabs((float)lFrame1.at<float>(i, j) - (float)lFrame2.at<float>(i, j));
+            motionImage.at<float>(i, j) = (fDiff >= MOTION_PRES) ? fDiff : 0.0f;
+        }
+    }
+    return motionImage;
+}
+
+OpticalVector *getOpticalFlow(Mat *frame1, Mat *frame2, int width, int height)
+{
+    // Get the luminance of the two frames
+    Mat1f lFrame1 = getLuminance(frame1[0], frame1[1], frame1[2], width, height);
+    Mat1f lFrame2 = getLuminance(frame2[0], frame2[1], frame2[2], width, height);
+    // Get the motionFrame of the frames
+    Mat1f motionFrame = getMotionImage(lFrame1, lFrame2, width, height);
+    // Create array of OpticalVector for the optical flow
+    struct OpticalVector *optFlow = (OpticalVector *)malloc(width * height * sizeof(struct OpticalVector));
+    // Declare the variables for the optical flow
+    float fPatchDifferenceMax = INFINITY;
+    float fPatchDifferenceX = 0.0f, fPatchDifferenceY = 0.0f;
+    int searchVectorX = 0, searchVectorY = 0;
+    float fAccumDif = 0.0f;
+    int patchPixelX = 0, PatchPixelY = 0;
+    int basePixelX = 0, basePixelY = 0;
+    float fPatchPixel = 0.0f, fBasePixel = 0.0f;
+    // Iterate over each pixel of the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Initialize the vector
+            optFlow[i * width + j].x = 0.0f;
+            optFlow[i * width + j].y = 0.0f;
+            // Initialize the variables
+            fPatchDifferenceMax = INFINITY;
+            fPatchDifferenceX = 0.0f;
+            fPatchDifferenceY = 0.0f;
+
+            // Search over a rectangular area for a patch of the image
+            //  Sx and Sy indicates the area of search
+            for (int sy = 0; sy < SEARCH_SIZE; sy++)
+            {
+                for (int sx = 0; sx < SEARCH_SIZE; sx++)
+                {
+                    // vectors that iterate over the search square
+                    searchVectorX = j + (sx - SEARCH_SIZE / 2);
+                    searchVectorY = i + (sy - SEARCH_SIZE / 2);
+
+                    // Variable to store the difference of the patch
+                    fAccumDif = 0.0f;
+
+                    // Iterate over the patch with px and py
+                    for (int py = 0; py < PATCH_SIZE; py++)
+                    {
+                        for (int px = 0; px < PATCH_SIZE; px++)
+                        {
+                            // Iterate over the patch
+                            patchPixelX = searchVectorX + (px - PATCH_SIZE / 2);
+                            PatchPixelY = searchVectorY + (py - PATCH_SIZE / 2);
+
+                            // Iterate over the patch of the original pixel
+                            basePixelX = j + (px - PATCH_SIZE / 2);
+                            basePixelY = i + (py - PATCH_SIZE / 2);
+
+                            // Get adjacent values for each patch checking that is inside the image
+                            fPatchPixel = 0.0f;
+                            if (patchPixelX >= 0 && patchPixelX < width && PatchPixelY >= 0 && PatchPixelY < height)
+                                fPatchPixel = lFrame2.at<float>(PatchPixelY, patchPixelX);
+
+                            fBasePixel = 0.0f;
+                            if (basePixelX >= 0 && basePixelX < width && basePixelY >= 0 && basePixelY < height)
+                                fBasePixel = lFrame1.at<float>(basePixelY, basePixelX);
+
+                            // Accumulate difference
+                            fAccumDif += fabs(fPatchPixel - fBasePixel);
+                        }
+                    }
+
+                    /*
+                    Record the vector offset for the least different search patch
+                    */
+                    if (fAccumDif <= fPatchDifferenceMax)
+                    {
+                        fPatchDifferenceMax = fAccumDif;
+                        optFlow[i * width + j].x = (float)(searchVectorX - j);
+                        optFlow[i * width + j].y = (float)(searchVectorY - i);
+                    }
+                }
+            }
+        }
+    }
+
+    // Check if the OpticalFrame detected has motion
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            optFlow[i].x *= motionFrame.at<float>(i, j) > 0 ? 1.0f : 0.0f;
+            optFlow[i].y *= motionFrame.at<float>(i, j) > 0 ? 1.0f : 0.0f;
+        }
+    }
+    return optFlow;
+}
+
+Mat *interpolateFrames(Mat *frame1, Mat *frame2, int width, int height)
+{
+    // Declare the Matrix for the intermediate frame
+    Mat *interFrame = new Mat[3]{frame1[0], frame1[1], frame1[2]};
+    // Declare the array for the Optical Flow
+    OpticalVector *opticalFlow = getOpticalFlow(frame1, frame2, width, height);
+    OpticalVector *opticalFlow2 = getOpticalFlow2(frame1, frame2, width, height);
+
+    // Create the new frame interpolating the optical Flow
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            /*
+            interFrame[0].at<uchar>((int)(i + opticalFlow[i * width + j].y / 2), (int)(j + opticalFlow[i * width + j].x / 2)) = frame1[0].at<uchar>(i, j);
+            interFrame[1].at<uchar>((int)(i + opticalFlow[i * width + j].y / 2), (int)(j + opticalFlow[i * width + j].x / 2)) = frame1[1].at<uchar>(i, j);
+            interFrame[2].at<uchar>((int)(i + opticalFlow[i * width + j].y / 2), (int)(j + opticalFlow[i * width + j].x / 2)) = frame1[2].at<uchar>(i, j);
+            */
+
+            
+
+             interFrame[0].at<uchar>(i + opticalFlow2[i * width + j].v, j + opticalFlow2[i * width + j].v) = frame1[0].at<uchar>(i, j);
+             interFrame[1].at<uchar>(i + opticalFlow2[i * width + j].v, j + opticalFlow2[i * width + j].v) = frame1[1].at<uchar>(i, j);
+             interFrame[2].at<uchar>(i + opticalFlow2[i * width + j].v, j + opticalFlow2[i * width + j].v) = frame1[2].at<uchar>(i, j);
+
+             
+            //if (opticalFlow[i * width + j]. > 0)
+                //std::cout << opticalFlow[i * width + j].x << ":" << opticalFlow[i * width + j].x << "  -  " << opticalFlow2[i * width + j].u << ":" << opticalFlow[i * width + j].v << std::endl;
+        }
+    }
+    return interFrame;
 }

fa536b525758e30beda3b19395a8ad99fb96ba41
Juan Sebastian <juarodriguezc@unal.edu.co>
2022-11-09 02:25:53 -0500

    CUDA colab

--- proyecto_final/CUDA/script.sh
+++ proyecto_final/CUDA/script.sh
@@ -31,14 +31,13 @@ else
 
     echo "Verificando la ejecución de Cmake ..."
     #Check if cmake command has already been executed
-    if [ ! -f "MotionInterpolation" ]; then
-      [ -e "cmake_install.cmake" ] && rm "cmake_install.cmake"
-      [ -e "CMakeCache.txt" ] && rm "CMakeCache.txt"
-      rm -rf "build"
-      rm -rf "Makefile"
-      rm -rf "CMakeFiles"
-      cmake .
-    fi
+    [ -e "cmake_install.cmake" ] && rm "cmake_install.cmake"
+    [ -e "CMakeCache.txt" ] && rm "CMakeCache.txt"
+    rm -rf "build"
+    rm -rf "Makefile"
+    rm -rf "CMakeFiles"
+    cmake .
+    
     echo "Compilando el programa ..."
     make
     if [ $? -eq 0 ]; then

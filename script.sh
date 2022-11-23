9b63671618d72da974114e8319f262f5eaa9161a
Juan Sebastian <juarodriguezc@unal.edu.co>
2022-11-23 17:58:50 -0500

    FIx path

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ do
   for (( i=1; i<=(16); i=i*2 ))
   do
     echo ""
-    mpirun -np $i --hostfile mpi_hosts ./MotionInterpolation /src/video/test"$test"/ test.mp4 result.avi 80 0
+    #mpirun -np $i --hostfile mpi_hosts ./MotionInterpolation /src/video/test"$test"/ test.mp4 result.avi 80 0
   done
 done
+mpirun -np 4 --hostfile mpi_hosts ./MotionInterpolation /src/video/test4/ test.mp4 result.avi 80 0

714f46de5930552ba5bc7e7261cef9f00d57aabf
Juan Sebastian <juarodriguezc@unal.edu.co>
2022-11-23 23:13:15 -0500

    Fix GCD version

--- script.sh
+++ script.sh
@@ -5,7 +5,6 @@ do
   for (( i=1; i<=(16); i=i*2 ))
   do
     echo " Test #$test  -  #Process: $i "
-    mpirun -np $i --hostfile mpi_hosts ./MotionInterpolation /src/video/test"$test"/ test.mp4 result.avi 80 0
+    mpirun -np $i --hostfile mpi_hosts ./MotionInterpolation /home/"$USER"/Sistemas-distribuidos/src/video/test"$test"/ test.mp4 result.avi 80 0
   done
 done
-mpirun -np 4 --hostfile mpi_hosts ./MotionInterpolation /home/"$USER"/Sistemas-distribuidos/src/video/test4/ test.mp4 result.avi 8 0

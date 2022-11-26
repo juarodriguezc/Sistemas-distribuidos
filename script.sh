ffbba01dfa555812b5cc8895b8247dcb79018d5b
Juan Sebastián Rodríguez Castellanos <64822765+juarodriguezc@users.noreply.github.com>
2022-11-26 13:59:19 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ do
     for threads in {1,2,3,4}
     do
       echo " Test #$test  -  #Process: $process:   -   #Threads: $threads"
-      mpirun -np $process --hostfile mpi_hosts ./MotionInterpolation /home/"$USER"/Sistemas-distribuidos/src/video/test"$test"/ test.mp4 result.avi 80 0 $threads
+      mpirun -np $process --hostfile mpi_hosts ./MotionInterpolation /home/"$USER"/Sistemas-distribuidos/src/video/test"$test"/ test.mp4 result.avi 25 0 $threads
       #mpirun -np $process --hostfile mpi_hosts ./MotionInterpolation src/video/test"$test"/ test.mp4 result.avi 10 0 $threads
     done   
   done
